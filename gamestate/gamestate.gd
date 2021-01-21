6f244d4c76b7d19ce81c6bfd40e6b943bc0f2b4f
Cofla <alexis_g90@hotmail.com>
2021-01-21 17:12:44 -0300

    server wipeado

--- gamestate/gamestate.gd
+++ gamestate/gamestate.gd
@@ -28,6 +28,7 @@ func create_server():
 
 # Callback from SceneTree, called when client connects
 func _player_connected(_id):
+	registrar_jugador(_id)
 	emit_signal("cliente_conectado", _id)
 	print("Client ", _id, " connected")
 
@@ -36,10 +37,41 @@ func _player_connected(_id):
 func _player_disconnected(_id):
 	if ready_players.has(_id):
 		ready_players.erase(_id)
-		# rpc("unregister_player", _id)
+		#rpc("unregister_player", _id)
+	players.erase(_id)
+	for peer_id in players:
+		rpc_id(0, "unregister_jugador", _id)
 
+	rpc_id(0,"update_player_list") # not needed, just double checks everyone on same page
+	print(players)
+	
 	emit_signal("cliente_desconectado", _id)
 	print("Client ", _id, " disconnected")
 
-func iniciar_partida(partida):
-	Orquestador.iniciar_partida(partida)
+func registrar_jugador(id): 
+	print("Everyone sees this.. adding this id to your array! ", id) # everyone sees this
+	#the server will see this... better tell this guy who else is already in...
+	#if !(id in players):
+	players[id] = ""
+	
+	
+	# Send the info of existing players to the new player from ther server's personal list
+	for peer_id in players:
+		rpc_id(id, "registrar_jugador", peer_id) #rpc_id only targets player with specified ID			
+	
+	rpc_id(0,"update_player_list") # not needed, just double checks everyone on same page
+	
+	print(players)
+
+remote func unregister_jugador(id):
+	players.erase(id)	
+
+remote func update_player_list():
+		for x in players:
+			print(x)
+	
+
+func iniciar_partida(_partida):
+	for peer_id in players:
+		rpc_id(peer_id,"iniciar_partida", _partida)
+	#Orquestador.iniciar_partida(partida)
