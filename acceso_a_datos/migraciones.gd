76baaf7d55365136e496c085e00097c8f3cd2d28
bulfonjf <bulfonjf@gmail.com>
2021-01-23 16:14:03 -0300

    Se agregan migraciones
    Terrenos: los terrenos estaran en la db y tamb el coste de movimiento
    del tipo de la unidad por cada terreno
    Slots: hay una tabla para todos los slots.
      slots por tipo de unidad: se crea tabla intermedia.
      slots por item: se crea tabla intermedia.
    Para saber que tiene equipado una unidad ver tabla items_equipados, cada
    fila en esta tabla es por slot, por ej:
      id_unidad 1 (ej fighter), id_slot 1 (ej mano derecha), id_item 1 (ej espada)
      si un item ocupa dos slots entonces habra dos registros en esta tabla
        id_unidad 1 (ej fighter), id_slot 1 (ej mano derecha), id_item 2 (ej
      hacha de dos manos)
        id_unidad 1 (ej fighter), id_slot 2 (ej mano izquierda), id_item 2 (ej
      hacha de dos manos)
      de esta forma tamb sabemos todos los slots que tiene ocupados una
      unidad.

--- acceso_a_datos/migraciones.gd
+++ acceso_a_datos/migraciones.gd
@@ -11,10 +11,18 @@ func aplicar_migraciones():
 		crear_turnos(),
 		crear_jugadores(),
 		crear_facciones(),
-		crear_unidades(),
+		crear_terrenos(),
+		crear_slots(),
+		crear_tipo_unidades(),
+		crear_slots_tipo_unidades(),
+		crear_coste_movimiento_tipo_unidad(),
 		crear_items(),
+		crear_slots_item(),
+		crear_unidades(),
 		crear_items_equipados(),
-		crear_celdas_ocupadas()
+		crear_celdas_ocupadas(),
+		insertar_slots(),
+		insertar_items()
 		]
 	
 	var transaccion_db = transaccion_script.new()
@@ -76,31 +84,84 @@ func crear_facciones():
 		FOREIGN KEY (id_jugador) REFERENCES jugadores(id)
 		);"""
 
-func crear_unidades():
+func crear_terrenos():
 	return """
-		create table if not exists unidades (
-		id integer PRIMARY KEY,
-		id_faccion integer,
-		tipo nvarchar(500),
-		vida integer,
-		ataque_base integer,
-		defensa_base integer,
+		create table if not exists terrenos(
+			id integer PRIMARY KEY,
+			nombre nvarchar(500)
+		);"""
 
-		FOREIGN KEY (id_faccion) REFERENCES facciones(id)
+func crear_slots():
+	return """
+		create table if not exists slots(
+			id integer PRIMARY KEY,
+			nombre nvarchar(500)
+		);"""
+
+func crear_tipo_unidades():
+	return """
+		create table if not exists tipo_unidades (
+			id integer PRIMARY KEY,
+			nombre nvarchar(500),
+			vida integer,
+			ataque_base integer,
+			defensa_base integer,
+			movimientos integer
+			);"""
+
+func crear_slots_tipo_unidades():
+	return """
+		create table if not exists slots_tipo_unidades(
+			id_slot integer,
+			id_tipo_unidad integer,
+
+			FOREIGN KEY (id_slot) REFERENCES slots(id)
+			FOREIGN KEY (id_tipo_unidad) REFERENCES tipo_unidades(id)
+		);"""
+
+func crear_coste_movimiento_tipo_unidad():
+	return """
+		create table if not exists coste_movimiento_tipo_unidad(
+			id_tipo_unidad integer,
+			id_terreno integer,
+			coste_movimiento integer
+
+			FOREIGN KEY (id_tipo_unidad) REFERENCES tipo_unidades(id)
+			FOREIGN KEY (id_terreno) REFERENCES terrenos(id)
 		);"""
 
 func crear_items():
 	return """
 		create table if not exists items (
 		id integer PRIMARY KEY,
-		id_partida integer,
 		nombre nvarchar(500),
+		vida integer,
+		ataque_base integer,
+		defensa_base integer
+		);"""
 
+func crear_slots_item():
+	return """
+		create table if not exists slots_item (
+			id_item integer,
+			id_slot integer
+
+			FOREIGN KEY (id_item) REFERENCES items(id)
+			FOREIGN KEY (id_slot) REFERENCES slots(id)
+		);"""
+
+func crear_unidades():
+	return """
+		create table if not exists unidades (
+		id integer PRIMARY KEY,
+		id_faccion integer,
+		id_tipo integer,
 		vida integer,
 		ataque_base integer,
 		defensa_base integer,
 
-		FOREIGN KEY (id_partida) REFERENCES partidas(id)
+		FOREIGN KEY (id_faccion) REFERENCES facciones(id)
+		FOREIGN KEY (id_tipo) REFERENCES tipo_unidades(id)
 		);"""
 
 func crear_items_equipados():
@@ -108,9 +169,11 @@ func crear_items_equipados():
 		create table if not exists items_equipados (
 		id_unidad integer,
 		id_item integer,
+		id_slot integer
 
 		FOREIGN KEY (id_unidad) REFERENCES unidades(id)
 		FOREIGN KEY (id_item) REFERENCES items(id)
+		FOREIGN KEY (id_slot) REFERENCES slots_tipo_unidades(id)
 		);"""
 
 func crear_celdas_ocupadas():
@@ -121,3 +184,22 @@ func crear_celdas_ocupadas():
 			entidad_id integer,
 			entidad_tipo nvarchar(500)
 		);"""
+
+func insertar_slots():
+	return """
+		insert into slots(1, 'pecho');
+		insert into slots(2, 'mano_izquierda');
+		insert into slots(3, 'mano_derecha');
+		insert into slots(4, 'caballo');
+	"""
+
+func insertar_items():
+	return """
+		
+		insert into items(1, 'pechera de cuero', 0, 0, 10);
+		insert into slots_item(1,1)
+		insert into items(2, 'espada', 0, 10, 0);
+		insert into slots_item(2,3)
+		insert into items(3, 'armadura de caballo', 0, 0, 10);
+		insert into slots_item(3,4)
+	"""
